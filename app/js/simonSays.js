62b05eced0e5923aeb5ec7d8e4cf23535a84e1e6
NotNoper <n.attilako@gmail.com>
2024-02-13 13:21:30 +0200

    kinda did simon says

--- app/js/simonSays.js
+++ app/js/simonSays.js
@@ -8,19 +8,34 @@ function SimonSays(serial, colour)
         }
         else if(colour == "b")
         {
-
+            return "red"
         }
         else if(colour == "g")
         {
-
+            return "yellow"
         }
         else if(colour == "y")
         {
-
+            return "green"
         }
     }
     else if(!serial)
     {
-
+        if(colour == "r")
+        {
+            return "blue"
+        }
+        else if(colour == "b")
+        {
+            return "yellow"
+        }
+        else if(colour == "g")
+        {
+            return "green"
+        }
+        else if(colour == "y")
+        {
+            return "red"
+        }
     }
 }
