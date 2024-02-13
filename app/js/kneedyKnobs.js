24c0f5c51b9d7eda936895a5966be1db524f757e
NotNoper <n.attilako@gmail.com>
2024-02-13 13:03:10 +0200

    help how to check if text is written and then print the second one in list

--- app/js/kneedyKnobs.js
+++ app/js/kneedyKnobs.js
@@ -1,3 +1,9 @@
 const knobsPossibles = {
-    "no yes no yes": "up", "yes no no yes": "up", 
+    "no yes no yes": "up", "yes no no yes": "up", "no yes yes yes": "down", "yes no no yes": "down",
+    "no yes no no": "left", "no no no no": "left", "yes yes no yes": "right"
+};
+
+function knob(list)
+{
+    
 }
