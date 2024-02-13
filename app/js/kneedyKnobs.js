be5989217463ba2cd3814af6d57daecd0d323c76
NotNoper <n.attilako@gmail.com>
2024-02-13 13:14:53 +0200

    started sim sasy

--- app/js/kneedyKnobs.js
+++ app/js/kneedyKnobs.js
@@ -1,5 +1,5 @@
 const knobsPossibles = {
-    "no yes no yes": "up", "yes no no yes": "up", "no yes yes yes": "down", "yes no no yes": "down",
+    "no yes no yes": "up", "yes no no yes yes yes": "up", "no yes yes yes": "down", "yes no no yes yes no": "down",
     "no yes no no": "left", "no no no no": "left", "yes yes no yes": "right"
 };
 
---
+++ app/js/simonSays.js
@@ -0,0 +1,26 @@
+function SimonSays(serial, colour)
+{
+    if(serial)
+    {
+        if(colour == "r")
+        {
+            return "blue"
+        }
+        else if(colour == "b")
+        {
+
+        }
+        else if(colour == "g")
+        {
+
+        }
+        else if(colour == "y")
+        {
+
+        }
+    }
+    else if(!serial)
+    {
+
+    }
+}
