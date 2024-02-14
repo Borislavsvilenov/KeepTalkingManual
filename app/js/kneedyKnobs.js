86a29d2bf44b8c6371a8ab2d8b8671481236c183
Bobislav <bobo.svilenov@gmail.com>
2024-02-14 11:50:05 +0200

    fixed knob

--- app/js/kneedyKnobs.js
+++ app/js/kneedyKnobs.js
@@ -1,5 +1,5 @@
 const knobsPossibles = {
-    'n l n l l l': "up", 'l n n l ll': "up",
+    'n l n l l l': "up", 'l n n l l l': "up",
     'n l l l l l': "down", "l n n l l n": "down",
     'n l n n n n': "left", "n n n n n n": "left",
     'l l n l l l': "right"
