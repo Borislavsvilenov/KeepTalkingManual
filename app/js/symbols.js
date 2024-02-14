24baed0cc7af2908c550bd537f530acecfe2a5fc
Bobislav <bobo.svilenov@gmail.com>
2024-02-14 12:51:48 +0200

    sym

--- app/js/symbols.js
+++ app/js/symbols.js
@@ -1,7 +1,21 @@
-function getInputs(msg) {
+const order = [['ol', 'at', 'lamda', 'n', 'SS', 'nl', 'Cdot'],
+               ['eU', 'ol', 'Cdot', 'sq', 'estar', 'nl', '?'],
+               ['Ccir', 'ear', 'sq', 'XI', '3', 'lamda', 'estar'],
+               []
+];
 
+
+function symInputs(msg) {
+    msg.splice(0, 1);
+
+    let sym1 = msg[0];
+    let sym2 = msg[1];
+    let sym3 = msg[2];
+    let sym4 = msg[3];
+
+    return [sym1, sym2, sym3, sym4];
 }
 
-function decodeOrder(symbols) {
+function decodeSymbols(sym1, sym2, sym3, sym4) {
     
 }
