1ebfff44178e2f5787086c9699800c7ef83788c0
NotNoper <n.attilako@gmail.com>
2024-02-14 09:36:42 +0200

    did button

--- app/js/theButton.js
+++ app/js/theButton.js
@@ -1,10 +1,47 @@
-function TheButton(label, colour, batteries, indicator)
+function TheButton(label, colour, batteries, indicator, litColour)
 {
     if(colour == "blue" && label == "abort")
     {
         return "Hold Button"
     }
-    else if(batteries > 1 ) {
-        
+    else if(batteries > 1 &&label == "detonate") {
+        return "Immediately press and release"
+    }
+    else if(colour == white && indicator == "CAR")
+    {
+        return "Hold Button"
+    }
+    else if(batteries > 2 && indicator == "FRK")
+    {
+        return "Press and immediately release"
+    }
+    else if(colour == "yellow")
+    {
+        return "Hold Button"
+    }
+    else if(colour == "red" && label == "hold")
+    {
+        return "Press and immediately release"
+    }
+    else
+    {
+        return "Hold Button"
+    }
+
+    if(litColour == "blue")
+    {
+        return "Release when 4 in any position"
+    }
+    else if(litColour == "white")
+    {
+        return "Release when 1 in any position"
+    }
+    else if(litColour == "yellow")
+    {
+        return "Release when 5 in any position"
+    }
+    else
+    {
+        return "Release when 1 in any position"
     }
 }
