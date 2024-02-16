dea6675ce290e4b64a7603d1c27c2bc2154b42d5
NotNoper <n.attilako@gmail.com>
2024-02-16 09:09:29 +0200

    rfe

--- app/js/sWires.js
+++ app/js/sWires.js
@@ -1,1 +1,2 @@
-const redSequence = {'1': 'cut if C', '2': 'cut if B'}
+const redSequence = {'1': 'cut if C', '2': 'cut if B', '3': "cut if A", "4": "cut if A or C",
+'5': "cut if B", '6': 'cut if A or C', '7': 'cut if A, B or C', '8': 'cut if A or B', '9': "cut if B"}
