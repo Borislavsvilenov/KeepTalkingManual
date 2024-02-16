7986e8e4f8cc9f7f1692aa4bee64d9c6e7bc8105
NotNoper <n.attilako@gmail.com>
2024-02-16 09:43:33 +0200

    fqgrehrty

--- app/js/sWires.js
+++ app/js/sWires.js
@@ -3,3 +3,6 @@ const redSequence = {'1': 'cut if C', '2': 'cut if B', '3': "cut if A", "4": "cu
 
 const blueSequence = {'1': 'cut if B', '2': "cut if A or C", '3': 'cut if B', '4': 'cut if A', '5': 'cut if B',
 '6': 'cut if B or C', '7': 'cut if C', '8': 'cut if A or C', '9': 'cut if A'}
+
+const blackSequence = {'1': 'cut if A, B or C', '2': 'cut if A or C', '3': 'cut if B', '4': 'cut if A or C',
+'5': 'cut if B', '6': 'cut if B  or C', "7": 'cut if A or B', '8': 'cut if C', '9': 'cut if C'}
