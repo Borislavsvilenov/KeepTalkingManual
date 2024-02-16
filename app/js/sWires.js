cf73b6e96abb61bc5b964f931cb40094885b6946
Bobislav <bobo.svilenov@gmail.com>
2024-02-16 10:16:14 +0200

    commiting all

--- app/js/sWires.js
+++ app/js/sWires.js
@@ -6,3 +6,36 @@ const blueSequence = {'1': 'cut if B', '2': "cut if A or C", '3': 'cut if B', '4
 
 const blackSequence = {'1': 'cut if A, B or C', '2': 'cut if A or C', '3': 'cut if B', '4': 'cut if A or C',
 '5': 'cut if B', '6': 'cut if B  or C', "7": 'cut if A or B', '8': 'cut if C', '9': 'cut if C'}
+
+function sequence(wires) {
+
+  wires.splice(0, 1);
+
+  let reds = 0;
+  let blues = 0;
+  let blacks = 0;
+
+  let current = wires[wires.length - 1];
+
+  for(let w = 0; w < wires.length; w++) {
+    if(wires[w] == 'red') {
+      reds++;
+    } else if(wires[w] == 'blue') {
+      blues++;
+    } else if(wires[w] == 'black') {
+      blacks++;
+    } 
+  }
+
+  if(current == 'red') {
+    return redSequence[reds.toString()];
+  } else if(current == 'blue') {
+    return blueSequence[blues.toString()];
+  } else if(current == 'black') {
+    return blackSequence[blacks.toString()];
+  }
+}
+
+
+
+
