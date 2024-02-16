dced009ec84d5b76aef420879eddff88d7581718
NotNoper <n.attilako@gmail.com>
2024-02-16 09:12:49 +0200

    cgbhjl

--- app/js/sWires.js
+++ app/js/sWires.js
@@ -1,2 +1,5 @@
 const redSequence = {'1': 'cut if C', '2': 'cut if B', '3': "cut if A", "4": "cut if A or C",
 '5': "cut if B", '6': 'cut if A or C', '7': 'cut if A, B or C', '8': 'cut if A or B', '9': "cut if B"}
+
+const blueSequence = {'1': 'cut if B', '2': "cut if A or C", '3': 'cut if B', '4': 'cut if A', '5': 'cut if B',
+'6': 'cut if B or C', '7': 'cut if C', '8': 'cut if A or C', '9': 'cut if A'}
